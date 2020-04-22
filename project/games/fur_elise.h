daeaef6a95a54d2b69b9d779a74249d14381f655
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-22 17:56:50 -0600

    Added game1 from proj2

---
+++ project/games/fur_elise.h
@@ -0,0 +1,15 @@
+// Nichole Maldonado
+// This is the header file for fur_elise.c
+// which contains functions to play the first
+// nine notes of fur elise and turn on
+// bright or dim lights accordingly.
+
+// Plays the first nine notes of
+// fur elise
+
+#ifndef fur_elise
+#define fur_elise
+
+void fur_elise_sound();
+
+#endif //included
