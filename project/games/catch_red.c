22d4bef9cc001bcab833fde52dc6c5d21e34cc38
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-22 17:56:14 -0600

    Added game3 from proj2

---
+++ project/games/catch_red.c
@@ -0,0 +1,51 @@
+// Nichole Maldonado
+// This file is for game 3 and alternates lights
+// based on the light_speed.
+
+#include "catch_red.h"
+#include "led.h"
+#include "buzzer.h"
+
+unsigned char light_speed = 150;
+
+static void alternate_lights();
+
+/*
+ * Function that alternates the lights 
+ * between red and green.
+ * Input: None
+ * Output: None
+ */
+static void alternate_lights()
+{
+  // Turn on red.
+  if (!red_on){
+    buzzer_set_period(1000, 1);
+    turn_on_red();
+  }
+
+  // Turn on green.
+  else {
+    buzzer_set_period(9000, 1);
+    turn_on_green();
+  }
+}
+
+/*
+ * Function that issues the alternating of the
+ * leds when the blink_count reaches the
+ * light_speed.
+ * Input: None.
+ * Output: None.
+ */
+void catch_red()
+{
+  static char blink_count = 0;
+
+  // When the blink_count equals the light_speed
+  // then alternate the lights.
+  if (++blink_count == light_speed) {
+    alternate_lights();
+    blink_count = 0;
+  }
+}
---
+++ project/games/catch_red.h
@@ -0,0 +1,10 @@
+// Nichole Maldonado
+// This is the header file for catch_red which
+// contains a function to alternate the light
+// when the count meets the light_speed.
+
+extern unsigned char light_speed;
+
+// Game that alternates lights based on
+// light_speed.
+void catch_red();
