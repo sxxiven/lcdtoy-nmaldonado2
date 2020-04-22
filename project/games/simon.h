ca1b69f46df425ebfaf3f333c7930132a3b62d48
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-22 17:57:16 -0600

    Added game4 from proj2

---
+++ project/games/simon.h
@@ -0,0 +1,17 @@
+// Nichole Maldonado
+// Header file for simon.c that contians the
+// functions for simon which shows a pattern
+// of lights and adds a pattern to game_pattern
+// if add_pattern is set to true. Note that
+// only 32 game patterns can be reached.
+
+extern unsigned char add_pattern;
+extern unsigned char curr_pattern;
+extern unsigned char wait_for_pattern;
+extern unsigned char game_pattern[32];
+
+// Function that initiates the printing of
+// game patterns and adding to the pattern.
+void simon();
+
+
