bd03a5861e8a5bd4811d03515744767cd237c0e9
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-22 18:00:17 -0600

    Added move layer code inseperate files

---
+++ project/move_layer.h
@@ -0,0 +1,18 @@
+extern u_int bgColor;
+extern int redrawScreen;
+extern Region fieldFence;
+
+/** Moving Layer
+ *  Linked list of layer references
+ *  Velocity represents one iteration of change (direction & magnitude)
+ */
+typedef struct MovLayer_s {
+  Layer *layer;
+  Vec2 velocity;
+  struct MovLayer_s *next;
+} MovLayer;
+
+
+void movLayerDraw(MovLayer *movLayers, Layer *layers);
+
+void mlAdvance(MovLayer *ml, Region *fence);
