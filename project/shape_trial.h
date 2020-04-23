ab85414a5517af7adeefb4314b135c458873efce
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-22 18:00:56 -0600

    Added objs from shape motion demo to check integration

---
+++ project/shape_trial.h
@@ -0,0 +1,15 @@
+//#include "move_layer.h"
+//#include "shape.h"
+
+extern MovLayer ml3;
+extern MovLayer ml1;
+extern MovLayer ml0;
+extern Layer fieldLayer;
+extern Layer layer0;
+extern Layer layer1;
+extern Layer layer2;
+extern Layer layer3;
+extern Layer layer4;
+extern AbRect rect10;
+extern AbRArrow rightArrow;
+extern AbRectOutline fieldOutline;
