87194a4e7f1443e62148652ba9720714cf9d22d0
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-24 10:56:12 -0600

    Removed display change to interrupt handler

--- project/display/find_frequency_display.c
+++ project/display/find_frequency_display.c
@@ -13,6 +13,7 @@
 #include "move_layer.h"
 #include "fur_elise_display.h"
 #include "hourglass.h"
+#include "display_utils.h"
 
 const ab_hourglass hourglass = {ab_hourglass_get_bounds, ab_hourglass_check, {12, 17}, 3};
 const ab_hourglass hourglass_mini = {ab_hourglass_get_bounds, ab_hourglass_check, {10, 15}, 0};
@@ -21,51 +22,81 @@ AbRArrow rightArrow2 = {abRArrowGetBounds, abRArrowCheck, 30};
 
 AbRectOutline fieldOutline2 = {	/* playing field */
   abRectOutlineGetBounds, abRectOutlineCheck,   
-  {screenWidth/2 - 10, screenHeight/2 - 10}
+  {screenWidth/2 - 5, screenHeight/2 - 5}
 };
 
 Layer fieldLayer_2 = {
   (AbShape *)&fieldOutline2,
   {(screenWidth/2), (screenHeight/2)}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_GREEN,
+  COLOR_BLACK,
   0
 };
 
-Layer layer3_3 = {
-  (AbShape *)&hourglass_mini,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
+Layer layer_hourglass_2 = {
+  (AbShape *)&hourglass,
+  {(screenWidth/2) -30, (screenHeight/2)-30}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_ORANGE,
+  COLOR_WHITE,
   &fieldLayer_2
 };
 
-Layer layer3_2 = {
+Layer layer_hourglass_1 = {
   (AbShape *)&hourglass,
-  {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
+  {(screenWidth/2)+30, (screenHeight/2)+30}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_PINK,
-  &layer3_3
+  COLOR_WHITE,
+  &layer_hourglass_2
 };
 
-MovLayer ml0_3 = {&layer3_3, {1,0}, 0};
-MovLayer ml0_2 = {&layer3_2, {1,2}, &ml0_3};
+Layer layer_mini_hourglass_2 = {
+  (AbShape *)&hourglass_mini,
+  {(screenWidth/2)-30, (screenHeight/2)-30}, /**< bit below & right of center */
+  {0,0}, {0,0},				    /* last & next pos */
+  COLOR_YELLOW,
+  &layer_hourglass_1
+};
+
+Layer layer_mini_hourglass_1 = {
+  (AbShape *)&hourglass_mini,
+  {(screenWidth/2)+30, (screenHeight/2)+30}, /**< bit below & right of center */
+  {0,0}, {0,0},				    /* last & next pos */
+  COLOR_RED,
+  &layer_mini_hourglass_2
+};
+
+MovLayer ml_hg_2 = {&layer_hourglass_2, {0,1}, 0};
+MovLayer ml_hg_1 = {&layer_hourglass_1, {0,1}, &ml_hg_2};
+MovLayer ml_mini_hg_2 = {&layer_mini_hourglass_2, {1,0}, &ml_hg_1};
+MovLayer ml_mini_hg_1 = {&layer_mini_hourglass_1, {1,0}, &ml_mini_hg_2};
 
 void display_new_find_frequency(){
-  layerInit(&layer3_2);
-  layerDraw(&layer3_2);
+  //fill_rectangle(0,0,screenWidth,screenHeight,COLOR_GREEN);
+  layerInit(&layer_mini_hourglass_1);
+  layerDraw(&layer_mini_hourglass_1);
   layerGetBounds(&fieldLayer_2, &fieldFence);
+  bgColor = COLOR_GREEN;
+}
+
+void change_color() {
+  static u_char color_pattern = 1;
+  
+  layer_mini_hourglass_1.color = (color_pattern) ? COLOR_ORANGE : COLOR_RED;
+  layer_mini_hourglass_2.color = (color_pattern) ? COLOR_ORANGE : COLOR_YELLOW;
+  layer_hourglass_1.color = layer_hourglass_2.color = (color_pattern) ? COLOR_BLACK : COLOR_WHITE;
+  color_pattern = (color_pattern) ? 0 : 1; 
+}
+
+void change_motion() {
+  static u_char motion = 0;
+  
+  ml_mini_hg_1.velocity.axes[0] = motion;
+  ml_mini_hg_2.velocity.axes[0] = motion;
+  ml_hg_1.velocity.axes[1] = motion;
+  ml_hg_2.velocity.axes[1] = motion;
+  motion = (motion) ? 0 : 1;
 }
 
 void find_frequency_display(char btn_pressed) {
-  /*switch(btn_pressed & 15) {
-  case 14:
-    bgColor = COLOR_RED;
-    display_new_find_frequency();
-    break;
-  default:
-    break;
-    
-  }*/
-  mlAdvance(&ml0_2, &fieldFence);
+  mlAdvance(&ml_mini_hg_1, &fieldFence);
 }
--- project/display/find_frequency_display.h
+++ project/display/find_frequency_display.h
@@ -1,13 +1,13 @@
 //#include "move_layer.h"
 //#include "shape.h"
 
-extern MovLayer ml0_2;
+extern MovLayer ml_mini_hg_1;
 
-extern Layer layer3_2;
+extern Layer layer_mini_hourglass_1;
 
-extern AbRect rect11;
-extern AbRArrow rightArrow2;
 extern AbRectOutline fieldOutline2;
 
 void find_frequency_display(char btn_pressed);
 void display_new_find_frequency();
+void change_color();
+void change_motion();
