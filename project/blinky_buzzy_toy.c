54d7cd4da9e87c311e1ca7cc7d3c95066c8fa812
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-27 12:07:03 -0600

    Updated games to use less memory

--- project/blinky_buzzy_toy.c
+++ project/blinky_buzzy_toy.c
@@ -14,22 +14,23 @@
 #include "state_machine_interrupt_handlers.h"
 #include "fur_elise_display.h"
 #include "find_frequency_display.h"
+#include "catch_red_ml.h"
 #include "catch_red_display.h"
-
+//#include "simon_display.h"
 void display_new_game() {
   switch(game_num) {
     case 1:
       display_new_piano();
       break;
     case 2:
-      keys_to_change = 0;
+      
       display_new_find_frequency();
       break;
     case 3:
       display_new_catch_red();
       break;
     default:
-      //
+      //    display_new_simon();
       break;
     }
  game_changed = 0;
@@ -38,7 +39,7 @@ void display_new_game() {
 void advance_draw() {
   switch(game_num) {
   case 1:
-    display_piano();
+    movLayerDraw(&ml_key_1, &key_1);
     break;
   case 2:
     movLayerDraw(&ml_mini_hg_1, &layer_mini_hourglass_1);
@@ -47,6 +48,7 @@ void advance_draw() {
     movLayerDraw(&ml_ball_1, &layer_ball_1);
     break;
   default:
+    //movLayerDraw(&ml_red_arrow, &layer_red_arrow);
     break;
   }
 }
@@ -62,7 +64,6 @@ int main(void) {
   // Sets the master oscillator, CPU, and clocks.
   configureClocks();
 
-  // Sets led, buzzer, and buttons.
   led_init();
 
   lcd_init();
@@ -70,33 +71,18 @@ int main(void) {
   buzzer_init();
   buttons_init();
 
-  shapeInit();
-
-  //layerInit(&layer0);
-  //layerDraw(&layer0);
-
   display_new_piano();
-  //layerGetBounds(&fieldLayer, &fieldFence);
-
-  
-  // Enables periodic intterupts.
   enableWDTInterrupts();
 
    for(;;) {
-    while (!redrawScreen && !game_changed) { //< Pause CPU if screen doesn't need updating 
-      //P1OUT &= ~GREEN_LED;    // Green led off witHo CPU 
+    while (!redrawScreen && !game_changed) {  
   or_sr(0x10);	      // CPU OFF
     }
-    // P1DIR |= BIT6;
-    //P1OUT |= GREEN_LED;       // Green led on when CPU on 
     redrawScreen = 0;
-    //movLayerDraw(&ml0_2, &layer1_2);
-    //display_piano();
     if (game_changed == 0) {
       advance_draw();
     }
     else {
-      //display_new_piano();
       display_new_game();
     }
   }
