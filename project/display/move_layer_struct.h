220f06ef98f2fc26c19dcd5e1ea38e0094a6c198
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-27 12:05:07 -0600

    relocated move_layer to display

---
+++ project/display/move_layer_struct.h
@@ -0,0 +1,12 @@
+#ifndef MOVE_LAYER_STRUCT
+#define MOVE_LAYER_STRUCT
+/** Moving Layer
+ *  Linked list of layer references
+ *  Velocity represents one iteration of change (direction & magnitude)
+ */
+typedef struct MovLayer_s {
+  Layer *layer;
+  Vec2 velocity;
+  struct MovLayer_s *next;
+} MovLayer;
+#endif
