fa13b2f0f6949ae4df8458e46eb46cfc37faf24c
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-29 11:46:40 -0600

    Fixed catch red for final display

--- project/display/catch_red_display.c
+++ project/display/catch_red_display.c
@@ -11,11 +11,8 @@
 #include "shape.h"
 #include <abCircle.h>
 #include "move_layer.h"
-#include "display_utils.h"
-#include "find_frequency_display.h"
 #include "catch_red_display.h"
-#include "catch_red_ml.h"
-
+/*
 Layer layer_ball_4 = {		
   (AbShape *)&circle14,
   {20, (screenHeight/2) - 52},
@@ -23,13 +20,14 @@ Layer layer_ball_4 = {
   COLOR_RED,
   &fieldLayer_2,
 };
+*/
 
 Layer layer_ball_3 = {		
   (AbShape *)&circle14,
   {(screenWidth/2)+30, (screenHeight/2)-18}, 
   {0,0}, {0,0},				    
   COLOR_GREEN,
-  &layer_ball_4,
+  &fieldLayer_2,
 };
 
 Layer layer_ball_2 = {		
@@ -48,26 +46,21 @@ Layer layer_ball_1 = {
   &layer_ball_2,
 };
 
-MovLayer ml_ball_4 = {&layer_ball_4, {2,0}, 0};
-MovLayer ml_ball_3 = {&layer_ball_3, {-2,0}, &ml_ball_4};
+//MovLayer ml_ball_4 = {&layer_ball_4, {2,0}, 0};
+MovLayer ml_ball_3 = {&layer_ball_3, {-2,0}, 0};
 MovLayer ml_ball_2 = {&layer_ball_2, {2,0}, &ml_ball_3};
 MovLayer ml_ball_1 = {&layer_ball_1, {-2,0}, &ml_ball_2};
 
 void display_new_catch_red(){
-  //fill_rectangle(0,0,screenWidth,screenHeight,COLOR_GREEN);
-  //and_sr(~8);
-  
   bgColor = COLOR_WHITE;
   layerInit(&layer_ball_1);
-  //fill_rectangle(0,0,screenWidth,screenHeight, bgColor);
   layerDraw(&layer_ball_1);
-  layerGetBounds(&fieldLayer_2, &fieldFence);
-  //or_sr(8);
+  
 }
 
 void change_ball_color() {
   static u_char color_pattern = 1;
-  layer_ball_4.color = layer_ball_2.color = (color_pattern) ? COLOR_GREEN : COLOR_RED;
+  layer_ball_2.color = (color_pattern) ? COLOR_GREEN : COLOR_RED;
     layer_ball_3.color = layer_ball_1.color = (color_pattern) ? COLOR_RED : COLOR_GREEN;
   color_pattern = (color_pattern) ? 0 : 1; 
 }
@@ -81,3 +74,4 @@ void catch_red_display(char btn_pressed) {
   }
   mlAdvance(&ml_ball_1, &fieldFence);
 }
+
--- project/display/catch_red_display.h
+++ project/display/catch_red_display.h
@@ -1,5 +1,10 @@
-//#include "move_layer.h"
-//#include "shape.h"
+#include "move_layer.h"
+#include "shape.h"
+
+extern MovLayer ml_ball_1;
+
+extern Layer layer_ball_1;
+
 
 void catch_red_display(char btn_pressed);
 void display_new_catch_red();
