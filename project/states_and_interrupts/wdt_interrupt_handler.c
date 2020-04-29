69c6e684a8e4975a145daf8fa105c342fe390440
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-29 12:16:33 -0600

    Fixed wdt for new states

--- project/states_and_interrupts/wdt_interrupt_handler.c
+++ project/states_and_interrupts/wdt_interrupt_handler.c
@@ -6,8 +6,6 @@
 
 #include <msp430.h>
 #include "state_machine_interrupt_handlers.h"
-//#include "fur_elise.h"
-
 #include "buttons.h"
 #include "libTimer.h"
 #include "lcdutils.h"
@@ -18,8 +16,6 @@
 #include "fur_elise_display.h"
 #include "find_frequency_display.h"
 #include "catch_red_display.h"
-//#include "simon_display.h"
-#include "catch_red_ml.h"
 #include "led.h"
 
 void display_game() {
@@ -32,16 +28,13 @@ void display_game() {
   if ((btn_pressed & 240) || game_num != 1){
     switch(game_num) {
     case 1:
-      mlAdvance(&ml_key_1, &fieldFence);
+      fur_elise_display(btn_pressed);
       break;
     case 2:
-      mlAdvance(&ml_mini_hg_1, &fieldFence);
+      mlAdvance(&ml_hg_1, &fieldFence);
       break;
     case 3:
       catch_red_display(btn_pressed);
-      break;
-    default:
-//  simon_display(btn_pressed);
       break;
     }
     redrawScreen = 1;
@@ -53,32 +46,14 @@ void display_game() {
 
 void wdt_c_handler()
 {
-//led_change();
   static short count = 0;
-  //P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
   if (count == 15) {
     display_game();
     count = 0;
+}
+  if (game_num == 2) {
+    turn_on_red();
+    turn_off_red();
   }
-
-  // Plays game based on game_num/state.
-/*
-switch(game_num) {
-  case 1:
-    
-    fur_elise_sound();
-    break;
-  case 2:
-//   find_frequency();
-    break;
-  case 3:
-//catch_red();
-break;
-  case 4:
-// simon();
-    break;
-  }
-*/    
-  // P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
 }
