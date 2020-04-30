0599237c2546efb262a532df41ef1fa35ca1d30a
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-29 18:37:46 -0600

    Commented display utils

--- project/display/display_utils.c
+++ project/display/display_utils.c
@@ -1,5 +1,18 @@
+// Nichole Maldonado
+// Function that fills a rectange of a
+// given height and width.
+
 #include "display_utils.h"
 #include "lcddraw.h"
+
+/*
+ * Draw a rectangle starting at min_col
+ * and min_row and taking up height and
+ * width.
+ * Input: The minimum column and row to draw the
+ * rectangle. The height, width and color of the rectangle.
+ * Output: None
+ */
 void fill_rectangle(u_char min_col, u_char min_row, u_char width, u_char height, u_int bgr) {
   for (u_char i = min_col; i < min_col + width; i++) {
     for (u_char j = min_row; j < min_row + height; j++) {
--- project/display/display_utils.h
+++ project/display/display_utils.h
@@ -1,3 +1,7 @@
+// Nichole Maldonado
+// Header for a function that fills a rectangle of a
+// given height and width.
+
 #include "lcdutils.h"
 
 void fill_rectangle(u_char min_col, u_char min_row, u_char width, u_char height, u_int bgr);
