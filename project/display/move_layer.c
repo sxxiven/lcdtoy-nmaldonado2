b740dbd31b16d67a9b80744cecd2e3ce793f8071
Nichole Maldonado <nmaldonado2@miners.utep.edu>
2020-04-29 18:38:06 -0600

    Commented move layer code

--- project/display/move_layer.c
+++ project/display/move_layer.c
@@ -1,3 +1,7 @@
+// Provided by the Instructor.
+// File that contains methods to
+// move the shapes and advance them.
+
 #include <msp430.h>
 #include "libTimer.h"
 #include "lcdutils.h"
@@ -5,38 +9,56 @@
 #include "shape.h"
 #include "move_layer.h"
 
-u_int bgColor = COLOR_GREEN;
-u_char redrawScreen = 1;
-Region fieldFence;
+u_int bg_color = COLOR_GREEN;
+u_char redraw_screen = 1;
+Region field_ence;
 
-void movLayerDraw(MovLayer *movLayers, Layer *layers)
+// Playing field
+AbRectOutline field_outline = {
+abRectOutlineGetBounds, abRectOutlineCheck,   
+{screenWidth/2 - 5, screenHeight/2 - 5}
+};
+
+Layer field_layer = {
+  (AbShape *)&field_outline,
+  {(screenWidth/2), (screenHeight/2)},
+  {0,0}, {0,0},				   
+  COLOR_BLACK,
+  0
+};
+
+/*
+ * Function that draws the shape movement.
+ * Input: the movLayers to move and their
+ *        corresponding layers.
+ * Output: None.
+ */
+void mov_layer_sraw(MovLayer *mov_layers, Layer *layers)
 {
   int row, col;
-  MovLayer *movLayer;
+  MovLayer *mov_layer;
 
   and_sr(~8);			/**< disable interrupts (GIE off) */
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Layer *l = movLayer->layer;
+  for (mov_layer = mov_layers; mov_layer; mov_layer = mov_layer->next) { /* for each moving layer */
+    Layer *l = mov_layer->layer;
     l->posLast = l->pos;
     l->pos = l->posNext;
   }
-  or_sr(8);			/**< disable interrupts (GIE on) */
-
-
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+  or_sr(8);		 
+  for (mov_layer = mov_layers; mov_layer; mov_layer = mov_layer->next) { /* for each moving layer */
     Region bounds;
-    layerGetBounds(movLayer->layer, &bounds);
+    layerGetBounds(mov_layer->layer, &bounds);
     lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
 		bounds.botRight.axes[0], bounds.botRight.axes[1]);
     for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
       for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
-	Vec2 pixelPos = {col, row};
+	Vec2 pixel_pos = {col, row};
 	u_int color = bgColor;
-	Layer *probeLayer;
-	for (probeLayer = layers; probeLayer; 
-	     probeLayer = probeLayer->next) { /* probe all layers, in order */
-	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
-	    color = probeLayer->color;
+	Layer *probe_layer;
+	for (probe_layer = layers; probe_layer; 
+	     probe_layer = probe_layer->next) { /* probe all layers, in order */
+	  if (abShapeCheck(probe_layer->abShape, &probe_layer->pos, &pixel_pos)) {
+	    color = probe_layer->color;
 	    break; 
 	  } /* if probe check */
 	} // for checking all layers at col, row
@@ -46,30 +68,27 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
   } // for moving layer being updated
 }	  
 
-
-
-//Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
-
 /** Advances a moving shape within a fence
  *  
  *  \param ml The moving shape to be advanced
  *  \param fence The region which will serve as a boundary for ml
  */
-void mlAdvance(MovLayer *ml, Region *fence)
+void ml_advance(MovLayer *ml, Region *fence)
 {
-  Vec2 newPos;
+  Vec2 new_pos;
   u_char axis;
-  Region shapeBoundary;
+  Region shape_boundary;
   for (; ml; ml = ml->next) {
-    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
-    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+    vec2Add(&new_pos, &ml->layer->posNext, &ml->velocity);
+    abShapeGetBounds(ml->layer->abShape, &new_pos, &shape_boundary);
     for (axis = 0; axis < 2; axis ++) {
-      if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
-	  (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
+      if ((shape_boundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
+	  (shape_boundary.botRight.axes[axis] > fence->botRight.axes[axis]) || shape_boundary.topLeft.axes[axis] < 5) {
 	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
-	newPos.axes[axis] += (2*velocity);
+	new_pos.axes[axis] += (2*velocity);
       }	/**< if outside of fence */
+     
     } /**< for axis */
-    ml->layer->posNext = newPos;
+    ml->layer->posNext = new_pos;
   } /**< for ml */
 }
--- project/display/move_layer.h
+++ project/display/move_layer.h
@@ -1,21 +1,27 @@
-#include "move_layer_struct.h"
+// Provided by the instructor.
+// Shape move functions provided by
+// the instructor.
 
-extern u_int bgColor;
-extern u_char redrawScreen;
-extern Region fieldFence;
+#ifndef MOVE_LAYER_STRUCT
+#define MOVE_LAYER_STRUCT
+
+extern u_int bg_color;
+extern u_char redraw_screen;
+extern Region field_fence;
+extern Layer field_layer;
 
 /** Moving Layer
  *  Linked list of layer references
  *  Velocity represents one iteration of change (direction & magnitude)
  */
-/*
 typedef struct MovLayer_s {
   Layer *layer;
   Vec2 velocity;
   struct MovLayer_s *next;
 } MovLayer;
-*/
 
-void movLayerDraw(MovLayer *movLayers, Layer *layers);
 
-void mlAdvance(MovLayer *ml, Region *fence);
+void mov_layer_draw(MovLayer *movLayers, Layer *layers);
+
+void ml_advance(MovLayer *ml, Region *fence);
+#endif
